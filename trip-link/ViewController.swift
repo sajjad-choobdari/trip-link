75d2f52eee13fa6aee8159cdcf0c28f33613ae68
Sajjad Choobdari <sajjad.choobdari@gmail.com>
2023-08-28 11:10:23 +0330

    Initial Commit

---
+++ trip-link/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trip-link
+//
+//  Created by Sajjad Choobdari on 8/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
