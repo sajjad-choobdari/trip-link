4cf15f99cea715f10d055b39a7d39b405d126b39
Sajjad Choobdari <sajjad.choobdari@gmail.com>
2023-09-02 10:52:25 +0330

    feat: contact screen view

--- trip-link/Controllers/ContactScreenVC.swift
+++ trip-link/Controllers/ContactScreenVC.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-//import Foundation
+import MobileCoreServices
+import UniformTypeIdentifiers
 
 enum ContactViewMode {
 	case view
@@ -18,14 +19,31 @@ protocol ContactScreenDelegate: AnyObject {
 	var contactViewMode: ContactViewMode { get set }
 }
 
-class ContactScreenVC: UITableViewController {
+class ContactScreenVC: UIViewController {
 		// Outlets
+	@IBOutlet weak var addPhotoButtonView: UIButton!
+	@IBOutlet weak var imageView: UIImageView!
 
 		// Variables
 	var contactViewMode = ContactViewMode.view
 	var cancelButton: UIBarButtonItem!
 	var doneButton: UIBarButtonItem!
 	var editButton: UIBarButtonItem!
+
+		// Life Cycles
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(onPressCancel))
+		doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(onPressDone))
+		editButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(onPressEdit))
+
+		updateActionButtons(for: self.contactViewMode)
+
+		if (self.contactViewMode == ContactViewMode.add) {
+			navigationItem.title = "New Contact"
+		}
+	}
 
 		// Methods
 	func updateActionButtons(for mode: ContactViewMode) {
@@ -49,6 +67,25 @@ class ContactScreenVC: UITableViewController {
 		if let navigationController = self.navigationController {
 			navigationController.popViewController(animated: true)
 		}
+	}
+
+	func hasPickedAnImage() -> Bool {
+		if let image = imageView.image {
+			if !image.isSymbolImage {
+				// The UIImageView has a non-nil image that is not a system symbol.
+				return true
+			}
+			return false
+		}
+		return false
+	}
+
+	func presentImagePicker() {
+		let imagePicker = UIImagePickerController()
+		imagePicker.delegate = self
+		imagePicker.sourceType = .photoLibrary
+		imagePicker.mediaTypes = [UTType.image.identifier]
+		present(imagePicker, animated: true, completion: nil)
 	}
 
 	func showDiscardChangesAlert() {
@@ -75,17 +112,6 @@ class ContactScreenVC: UITableViewController {
 		alert.addAction(keepEditingAction)
 
 		self.present(alert, animated: true, completion: nil)
-	}
-
-
-	// Life Cycles
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
-		cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(onPressCancel))
-		doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(onPressDone))
-		editButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(onPressEdit))
-		updateActionButtons(for: self.contactViewMode)
 	}
 
 	// Actions
@@ -116,5 +142,38 @@ class ContactScreenVC: UITableViewController {
 			// get confirmation from user for discarding changes and navigate back
 			showDiscardChangesAlert()
 		}
+	}
+
+
+	@IBAction func addPhoto(_ sender: UIButton) {
+		presentImagePicker()
+	}
+
+}
+
+extension ContactScreenVC: UIImagePickerControllerDelegate {
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		if let selectedImage = info[.originalImage] as? UIImage {
+			imageView.image = selectedImage
+			addPhotoButtonView.setTitle("Edit", for: .normal)
+
+		}
+		picker.dismiss(animated: true, completion: nil)
+	}
+
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		picker.dismiss(animated: true, completion: nil)
+	}
+}
+extension ContactScreenVC: UINavigationControllerDelegate {
+		//
+}
+
+
+
+class NegativePaddedTextView: UITextView {
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		textContainerInset = UIEdgeInsets(top: 0, left: -4, bottom: 0, right: -4)
 	}
 }
