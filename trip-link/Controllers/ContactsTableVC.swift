780fb91944ff24ef08f8c06e0eaf4596955a4262
Sajjad Choobdari <sajjad.choobdari@gmail.com>
2023-09-16 17:01:45 +0330

    refactor: changed contacts table from UIViewController to UITableViewController

--- trip-link/Controllers/ContactsTableVC.swift
+++ trip-link/Controllers/ContactsTableVC.swift
@@ -9,24 +9,38 @@ import UIKit
 
 let contactCellReuseIdentifier = "ContactCell"
 
-class ContactsTableVC: UIViewController {
-	// Outlets
-	@IBOutlet var tableView: UITableView!
+class ContactsTableVC: UITableViewController {
 
 	// Variables
 	private let contactsModel = Contacts()
 
 	// Methods
-	
+	func getFullName(contactItem: Contact) -> String {
+		var fullName = ""
+
+		if let firstName = contactItem.mutableProps.givenName {
+			fullName += firstName
+		}
+		if let lastName = contactItem.mutableProps.familyName {
+			if !fullName.isEmpty {
+				fullName += " "
+			}
+			fullName += lastName
+		}
+		if fullName.isEmpty {
+			fullName += "No Name"
+		}
+
+		return fullName
+	}
+
 	// Life Cycles
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		tableView.register(ContactsTableViewCell.self, forCellReuseIdentifier: contactCellReuseIdentifier)
-		tableView.backgroundColor = UIColor.clear
-		tableView.dataSource = self
-		tableView.delegate = self
 	}
+
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		tableView.reloadData()
@@ -46,64 +60,45 @@ class ContactsTableVC: UIViewController {
 		}
 	}
 
-	// Actions
-	func getFullName(contactItem: Contact) -> String {
-		var fullName = ""
-
-		if let firstName = contactItem.mutableProps.givenName {
-			fullName += firstName
-		}
-		if let lastName = contactItem.mutableProps.familyName {
-			if !fullName.isEmpty {
-				fullName += " "
-			}
-			fullName += lastName
-		}
-	if fullName.isEmpty {
-		fullName += "No Name"
-	}
-
-		return fullName
-	}
 }
 
-extension ContactsTableVC: UITableViewDataSource {
-	func numberOfSections(in tableView: UITableView) -> Int {
+extension ContactsTableVC {
+	override func numberOfSections(in tableView: UITableView) -> Int {
 		return 1
 	}
 
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return contactsModel.getItems().count
 	}
 
-	
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-	let cell = tableView.dequeueReusableCell(withIdentifier: contactCellReuseIdentifier, for: indexPath) as! ContactsTableViewCell
-//	cell = ContactsTableViewCell(style: .subtitle, reuseIdentifier: contactCellReuseIdentifier)
-	let contactItem = contactsModel.getItems()[indexPath.row]
-	cell.textLabel?.text = getFullName(contactItem: contactItem)
-	cell.detailTextLabel?.text = contactItem.mutableProps.phoneNumber
-	if let imageView = cell.imageView {
-		if let imageData = contactItem.mutableProps.image {
-			imageView.image = UIImage(data: imageData)
-		} else {
-			imageView.image = UIImage(systemName: "person.crop.circle")
-		}
-	}
-	return cell
-}
 
-	func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: contactCellReuseIdentifier, for: indexPath) as! ContactsTableViewCell
+			//	cell = ContactsTableViewCell(style: .subtitle, reuseIdentifier: contactCellReuseIdentifier)
+		let contactItem = contactsModel.getItems()[indexPath.row]
+		cell.textLabel?.text = getFullName(contactItem: contactItem)
+		cell.detailTextLabel?.text = contactItem.mutableProps.phoneNumber
+		if let imageView = cell.imageView {
+			if let imageData = contactItem.mutableProps.image {
+				imageView.image = UIImage(data: imageData)
+			} else {
+				imageView.image = UIImage(systemName: "person.crop.circle")
+			}
+		}
+		return cell
+	}
+
+	override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
 		return true
 	}
 
-	func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+	override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
 		return .none
 	}
 }
 
-extension ContactsTableVC: UITableViewDelegate {
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+extension ContactsTableVC {
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		performSegue(withIdentifier: "ShowContactDetailSegue", sender: indexPath)
 	}
 }
