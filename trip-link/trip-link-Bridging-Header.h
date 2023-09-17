ec3d87bb0ac0da7c746dddcb42ea6e1e82b4043d
Sajjad Choobdari <sajjad.choobdari@gmail.com>
2023-09-17 10:29:46 +0330

    feat: added neshan iOS SDK

---
+++ trip-link/trip-link-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  trip-link-Bridging-Header.h
+//  trip-link
+//
+//  Created by Sajjad Choobdari on 9/16/23.
+//
+
+#ifndef trip_link_Bridging_Header_h
+#define trip_link_Bridging_Header_h
+
+#import <NeshanMobileSDK/NeshanMobileSDK.h>
+
+#endif /* trip_link_Bridging_Header_h */
